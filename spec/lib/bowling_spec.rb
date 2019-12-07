656c9c7aed7bbf642d6ea30bf01221706299be5f
EC2 Default User <ec2-user@ip-172-31-19-136.us-east-2.compute.internal>
2019-12-07 05:09:30 +0000

    update commit

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -37,7 +37,7 @@ describe "ボウリングのスコア計算" do
                 # 合計を計算
                 @game.calc_score
                 # 期待する合計 ※()内はボーナス点
-                # 3 + 7 + 4 + (4) = 18
+                # 3 + 7 + (4) + 4 = 18
                 expect(@game.total_score).to eq 18
             end  
         end        
@@ -61,7 +61,7 @@ describe "ボウリングのスコア計算" do
         end
 
         context "最終フレームでスペアを取った場合" do
-            it "スペアボーナスが加算されないこと" do
+            it "スペアボーナスが加算されること" do
                 # 第一フレームで3点,7点のスペア
                 @game.add_score(3)
                 @game.add_score(7)
@@ -72,11 +72,92 @@ describe "ボウリングのスコア計算" do
                 # 最終フレームで3点,7点のスペア
                 @game.add_score(3)
                 @game.add_score(7)
+                # 最終フレームでスペアを取った場合、次の1投分のスコアも考慮する必要あり
+                @game.add_score(10)
                 # 合計を計算
                 @game.calc_score
                 # 期待する合計 ※()内はボーナス点
-                # 3 + 7 + 4 + (4) + 3 + 7 = 28
-                expect(@game.total_score).to eq 28
+                # 3 + 7 + (4) + 4 + 3 + 7 + 10 = 38
+                expect(@game.total_score).to eq 38
+            end
+        end
+
+        context "ストライクを取った場合" do
+        	it "ストライクボーナスが加算されること" do
+            	# 第一フレームでストライク
+            	@game.add_score(10)
+            	# 第二フレームで5点,4点
+            	@game.add_score(5)
+            	@game.add_score(4)
+            	# 以降は全てガター
+            	add_many_scores(16, 0)
+            	# 合計を計算
+            	@game.calc_score
+            	# 期待する合計 ※()内はボーナス点
+            	# 10 + (5 + 4) + 5 + 4 = 28
+            	expect(@game.total_score).to eq 28
+            end
+        end
+
+        context "ダブルを取った場合" do
+        	it "それぞれのストライクボーナスが加算されること" do
+            	# 第一フレームでストライク
+            	@game.add_score(10)
+            	# 第二フレームでストライク
+            	@game.add_score(10)
+            	# 第三フレームで5点,4点
+            	@game.add_score(5)
+            	@game.add_score(4)
+            	# 以降は全てガター
+            	add_many_scores(14, 0)
+            	# 合計を計算
+            	@game.calc_score
+            	# 期待する合計 ※()内はボーナス点
+            	# 10 + (10 + 5) + 10 + (5 + 4) + 5 + 4 = 53
+            	expect(@game.total_score).to eq 53
+            end
+        end
+
+        context "ターキーを取った場合" do
+        	it "それぞれのストライクボーナスが加算されること" do
+            	# 第一フレームでストライク
+            	@game.add_score(10)
+            	# 第二フレームでストライク
+            	@game.add_score(10)
+            	# 第三フレームでストライク
+            	@game.add_score(10)
+            	# 第四フレームで5点,4点
+            	@game.add_score(5)
+            	@game.add_score(4)
+            	# 以降は全てガター
+            	add_many_scores(12, 0)
+            	# 合計を計算
+            	@game.calc_score
+            	# 期待する合計 ※()内はボーナス点
+            	# 10 + (10 + 10) + 10 + (10 + 5) + 10 + (5 + 4) + 5 + 4 = 83
+            	expect(@game.total_score).to eq 83
+            end
+        end
+
+        context "最終フレームでストライクを取った場合" do
+        	it "ストライクボーナスが加算されること" do
+            	# 第一フレームでストライク
+            	@game.add_score(10)
+            	# 第二フレームで5点,4点
+            	@game.add_score(5)
+            	@game.add_score(4)
+            	# 3～9フレームは全てガター
+            	add_many_scores(14, 0)
+            	# 最終フレームでストライク
+            	@game.add_score(10)
+                # 最終フレームでストライクを取った場合、次の2投分のスコアも考慮する必要あり
+            	@game.add_score(10)
+            	@game.add_score(10)
+            	# 合計を計算
+            	@game.calc_score
+            	# 期待する合計 ※()内はボーナス点
+            	# 10 + (5 + 4) + 5 + 4 + 10 + 10 + 10 = 58
+            	expect(@game.total_score).to eq 58
             end
         end
     end
@@ -85,7 +166,7 @@ end
 #private
 # 複数回のスコア追加をまとめて実行する
 def add_many_scores(count, pins)
-        count.times do
+    count.times do
         @game.add_score(pins)
     end
 end
